cb6d9af5cf45250653a969c947c36637a3c7da18
Melmsie <austin@melmsie.com>
2020-04-22 10:16:59 -0400

    basic functionality should work

--- main.go
+++ main.go
@@ -1,26 +1,43 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
 
+type ShitPost struct {
+	Title string `json:"data.children[0].data.title"`
+	Text string `json:"data.children[0].data.selftext"`
+	Message string `json:"message"`
+}
+
 func main() {
-	httpclient := &http.Client{}
-	req, err := http.NewRequest("GET", "https://www.reddit.com/r/copypasta/top/.json?sort=top&t=week&limit=1", nil)
+	res, err := http.Get("https://reddit.com/r/copypasta/top/.json?sort=top&t=week&limit=1")
+	if err != nil {
+		log.Println("error:", err)
+		return
+	}
+	if res.StatusCode != 200 { // TODO: Why are we getting 429 after just a few reqs, not even close to the RL
+		log.Fatalln("Non-OK Status:", res.Status)
+	}
 
+	body, err := ioutil.ReadAll(res.Body)
 	if err != nil {
 		log.Println("error:", err)
 		return
 	}
 
-	res, err := httpclient.Do(req)
+	s, err := getData([]byte(body))
+	log.Println(s)
+}
 
+func getData(body []byte) (*ShitPost, error) {
+	var s = new(ShitPost)
+	err := json.Unmarshal(body, &s)
 	if err != nil {
 		log.Println("error:", err)
-		return
 	}
-	fmt.Println(res.Body)
-	defer res.Body.Close()
+	return s, err
 }
